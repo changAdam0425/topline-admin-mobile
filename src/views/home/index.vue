28e49ee5ee013c8967e7b1023df98dc051949f8c
changAdam0425 <18235644369@163.com>
2019-07-09 08:49:14 +0800

    组件完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,10 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div>home</div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'HomeApp'
 }
 </script>
-
 <style lang="less" scoped>
 </style>
